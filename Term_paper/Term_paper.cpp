2232812d6f3e0f4038cbbf2479c70b7bf7ae99bf
Max <PovertyOfSelf@outlook.com>
2021-03-07 15:00:14 +0300

    1) Добавлена функция создания доски  //покан е работает 2) Добавлена фукнкция вывода доски в консоль 3) Полностью инициализируется доска в массиве из символов //добавление рабочей версии потом

--- Term_paper/Term_paper.cpp
+++ Term_paper/Term_paper.cpp
@@ -1,8 +1,17 @@
 ﻿#include <stdio.h>
-#include <SDL.h>
+#include "board_functions.h"
 
-int main(int argc, char const* argv[]) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    printf_s("Hello, world!\n");
+int main(int argc, char const* argv[]) 
+{
+    char board[6][6] = { {'1', '*', '*' , '*' , '*' , '*' }
+                        ,{'2', '*', '*' , '*' , '*' , '*' }
+                        ,{'3', '*', '*' , '@' , '*' , '*' }
+                        ,{'4', '*', '*' , '*' , '*' , '*' }
+                        ,{'5', '*', '*' , '*' , '*' , '*' } 
+                        ,{ '  ', 'a', 'b', 'c', 'd', 'e'} };
+    char coordinates_string[6] = { 'a', 'b', 'c', 'd', 'e', '\0' };
+    int coordinates_columns[6];
+    //create_board(board, coordinates_string, coordinates_columns);
+    print_borad (board);
     return 0;
 }
---
+++ Term_paper/board_functions.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void create_board(char board[6][6], int coordinates_columns[]);
+
+void print_borad (char board[6][6]);
